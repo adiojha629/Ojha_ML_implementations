ea3902e1c3d908189697e611e850d60d2f8744cc
adiojha629 <adiojha629@gmail.com>
2020-05-02 19:45:51 -0500

    Create least_squared_regression.py
    implemented least squared regression line

---
+++ Least Squared Regression/least_squared_regression.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  2 19:28:04 2020
+Least squares regression 
+
+@author: adioj
+"""
+
+import pandas as pd #for data stripping
+import numpy as np #for math operations
+import matplotlib.pyplot as plt #For graphing the data
+
+data = pd.read_csv('data.csv') #Import the data
+X = data.iloc[:, 0] #Variable X gets all the x values (values in first column)
+Y = data.iloc[:, 1] #Variable Y gets all y values (values in second column)
+
+
+""" Building the model
+Least Squares regression actually has a formula; so not backpropagation needed
+"""
+X_mean = np.mean(X)
+Y_mean = np.mean(Y)
+m_numerator = 0
+m_denominator = 0
+
+for x,y in zip(X,Y):
+    m_numerator += (x-X_mean)*(y-Y_mean)
+    m_denominator += (x-X_mean)**2
+    
+m = m_numerator / m_denominator #Slope
+c = Y_mean - m*X_mean #Y-intercept
+
+print("Slope is " + str(m) + ", Y-intercept is " + str(c))
+
+Y_pred = m*X + c
+
+"""Show Data on Graph"""
+
+plt.scatter(X,Y)
+plt.plot(X,Y_pred,color = 'red')
+plt.show()
